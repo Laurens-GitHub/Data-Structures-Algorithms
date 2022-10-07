c19210bb456238c47147b598c37a11d340ae0132
Lauren Edwards <laurenedwardsmail@gmail.com>
2022-10-07 02:04:21 +0000

    sentence reversal problem initial commit

---
+++ Array Sequences/Sentence Reversal/sentence_reversal.py
@@ -0,0 +1,30 @@
+"""Function for reversing the order of words in a sentence"""
+# Given a string of words, reverse all the words. For example:
+
+# Given:
+
+# 'This is the best'
+
+# Return:
+
+# 'best the is This'
+
+# As part of this exercise you should remove all leading and trailing whitespace. So that inputs such as:
+
+# '  space here'  and 'space here      '
+
+# both become:
+
+# 'here space'
+
+def reverser(words):
+    split_words = words.split()
+    new_sentence = ""
+
+    while len(split_words) > 0:
+        new_sentence += (f'{split_words[-1]} ')
+        split_words.pop()
+
+    return new_sentence.strip()
+
+print(reverser('best the is This'))
