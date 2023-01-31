6a2c1b459da91072b73c5ecc08af568696d16f66
Lauren Edwards <Lauren@LEdwards.co>
2023-01-31 19:29:13 +0000

    adding hourglass_sum solution

---
+++ Array Sequences/Other/hourglass_sum.py
@@ -0,0 +1,21 @@
+# See prompt here: https://www.hackerrank.com/challenges/2d-array/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    result = -sys.maxsize
+
+    for i in range(4):
+        for j in range(4):
+            curr_sum = (arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2])
+
+            result = max(result, curr_sum)
+
+    return result
+
